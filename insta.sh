a31abadaa7de7a76c1f8258cb6e78e2b004626b4
root <jeffersonmonteiro485@gmail.com>
2017-01-03 16:38:47 -0200

    codigo

--- insta.sh
+++ insta.sh
@@ -1,7 +1,8 @@
 echo Otimização do sistema operacional ! 
 sleep 5
-apt-get remove --purge polari -y
-apt-get remove --purge rhythmbox -y
+apt-get autoremove --purge polari -y
+apt-get autoremove --purge rhythmbox -y
+apt-get autoremove --purge thunderbird * -y
 
 echo Limpando o Sistema ! 
 sleep 3
@@ -12,14 +13,6 @@ apt-get autoremove -y
 echo sistema Limpo 
 sleep 3 
 
-echo Atualizando o Sistema  
-sleep 3 
-apt-get update -y 
-apt-get upgrade -y 
-
-echo ... 
-echo sistema atualizado com sucesso 
-
 echo iniciando a instalação de programas 
 sleep 4 
 echo ...
@@ -29,10 +22,18 @@ echo configurando o vim
 echo "syntax on" >> /etc/vim/vimrc
 echo "set number" >> /etc/vim/vimrc
 
-apt-get install virtualbox 5.1 -y 
-apt-get install git -y 
+apt-get install clamtk -y 
+apt-get install bleachbit -y
 
-echo Limpando o Sistema ! 
+
+echo Atualizando o sistema apos a instalação dos pacotes 
+sleep 5
+echo ... 
+apt-get update -y 
+apt-get upgrade
+
+
+ echo Limpando o Sistema ! 
  sleep 3
  apt-get remove -y
  apt-get clean -y
@@ -41,6 +42,14 @@ echo Limpando o Sistema !
 
  echo sistema Limpo 
  sleep 5
+
+
+echo Atualizando o sistema apos a instalação dos pacotes 
+sleep 5
+echo ... 
+apt-get update -y
+apt-get upgrade
+
 
 echo Sistema pronto para uso ! 
 
